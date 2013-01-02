7758ce14efa5515e8eb10d20cafe9a0421f7f68b
Kevin Horek <kevin@kevinhorek.com>
2013-01-01 21:26:00 -0700

    Initial Commit

---
+++ CrystalBall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrystalBall
+//
+//  Created by Kevin Horek on 13-01-01.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrystalBall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrystalBall
+//
+//  Created by Kevin Horek on 13-01-01.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
